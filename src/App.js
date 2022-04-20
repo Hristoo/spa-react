82e09acff9fd8d8ae6ebf3073dee4514ac7d0559
Hristo Dimitrov <dimitrov_hristo@mail.bg>
2022-04-20 15:44:40 +0300

    uu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Route, Switch } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+import { Redirect } from "react-router-dom";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/product-detail/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
